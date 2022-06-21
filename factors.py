6969230656a643714874f4a8b6b8fa9681deefc1
BigDany1792 <dany1792dota@gmail.com>
2022-06-20 22:56:08 -0500

    ci: added permision

--- factors.py
+++ factors.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
+"""script that factorize as many numbers as possible into a
+   product of two smaller numbers"""
+
 
 def descom_number(num):
-    """"""
+    """Extracts the first prime factor of the number, and displays the
+       output formatted with the 2 respective factors.
+
+    Args:
+        num (int): the number that will be decomposed.
+    Attributes:
+        tmp (int): stores the number that will be decomposed so as not
+                   to lose the value.
+        factor_primo: the number with which we will be dividing
+    """
     tmp = num
     factor_primo = 2
+
     while num > 1:
         if num % factor_primo == 0:
             num /= factor_primo
             print(f"{tmp}={int(num)}*{factor_primo}")
             break
         else:
-            factor_primo+=1
+            factor_primo += 1
 
 
-file = open('tests/rsa-1')
+file = open('tests/test00')
 listlines = file.readlines()
+
 for line in listlines:
     descom_number(int(line))
